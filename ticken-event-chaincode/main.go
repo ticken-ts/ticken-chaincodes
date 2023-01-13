5ebb071e05bf83a17129eb4c13c90babe2df65ce
facundotorraca <facundo@innovid.com>
2023-01-13 00:52:34 -0300

    modifications to deploy chaincode as a service

--- ticken-event-chaincode/main.go
+++ ticken-event-chaincode/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"github.com/hyperledger/fabric-chaincode-go/shim"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"log"
+	"os"
+	"strconv"
 	"ticken-event-contract/ticken-event"
 )
 
+type serverConfig struct {
+	CCID    string
+	Address string
+}
+
 func main() {
+	config := serverConfig{
+		CCID:    os.Getenv("CHAINCODE_ID"),
+		Address: os.Getenv("CHAINCODE_SERVER_ADDRESS"),
+	}
+
 	// add metadata and init transaction context
 	tickenEventContract := new(ticken_event.Contract)
 	tickenEventContract.Info.Version = "0.0.1"
@@ -17,8 +31,75 @@ func main() {
 		panic(err.Error())
 	}
 
-	err = cc.Start()
-	if err != nil {
-		panic(err.Error())
+	//err = cc.Start()
+	//if err != nil {
+	//	panic(err.Error())
+	// }
+
+	server := &shim.ChaincodeServer{
+		CCID:     config.CCID,
+		Address:  config.Address,
+		CC:       cc,
+		TLSProps: getTLSProperties(),
 	}
+
+	if err := server.Start(); err != nil {
+		log.Panicf("error starting asset-transfer-basic chaincode: %s", err)
+	}
+}
+
+func getTLSProperties() shim.TLSProperties {
+	// Check if chaincode is TLS enabled
+	tlsDisabledStr := getEnvOrDefault("CHAINCODE_TLS_DISABLED", "true")
+	key := getEnvOrDefault("CHAINCODE_TLS_KEY", "")
+	cert := getEnvOrDefault("CHAINCODE_TLS_CERT", "")
+	clientCACert := getEnvOrDefault("CHAINCODE_CLIENT_CA_CERT", "")
+
+	// convert tlsDisabledStr to boolean
+	tlsDisabled := getBoolOrDefault(tlsDisabledStr, false)
+	var keyBytes, certBytes, clientCACertBytes []byte
+	var err error
+
+	if !tlsDisabled {
+		keyBytes, err = os.ReadFile(key)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+		certBytes, err = os.ReadFile(cert)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+	}
+	// Did not request for the peer cert verification
+	if clientCACert != "" {
+		clientCACertBytes, err = os.ReadFile(clientCACert)
+		if err != nil {
+			log.Panicf("error while reading the crypto file: %s", err)
+		}
+	}
+
+	return shim.TLSProperties{
+		Disabled:      tlsDisabled,
+		Key:           keyBytes,
+		Cert:          certBytes,
+		ClientCACerts: clientCACertBytes,
+	}
+}
+
+func getEnvOrDefault(env, defaultVal string) string {
+	value, ok := os.LookupEnv(env)
+	if !ok {
+		value = defaultVal
+	}
+	return value
+}
+
+// Note that the method returns default value if the string
+// cannot be parsed!
+func getBoolOrDefault(value string, defaultVal bool) bool {
+	parsed, err := strconv.ParseBool(value)
+	if err != nil {
+		return defaultVal
+	}
+	return parsed
 }
