0caa5d818d221a150c397a41669ee3402ea6a75e
facundotorraca <facundo@innovid.com>
2023-01-23 21:21:06 -0300

    add ticken context in common package

---
+++ common/context.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type ITickenTxContext interface {
+	contractapi.TransactionContextInterface
+	GetInvoker(chaincode string) *Invoker
+}
+
+type TickenTxContext struct {
+	contractapi.TransactionContext
+	invokers map[string]*Invoker
+}
+
+func NewTransactionContext() ITickenTxContext {
+	ctx := new(TickenTxContext)
+	ctx.invokers = make(map[string]*Invoker)
+	return ctx
+}
+
+func (ctx *TickenTxContext) GetInvoker(chaincode string) *Invoker {
+	invoker, ok := ctx.invokers[chaincode]
+	if !ok {
+		invoker = NewInvoker(chaincode, ctx.GetStub())
+		ctx.invokers[chaincode] = invoker
+	}
+	return invoker
+}
--- ticken-event-chaincode/ccnotifier/interfaces.go
+++
@@ -1,8 +0,0 @@
-package ccnotifier
-
-import "ticken-event-contract/models"
-
-type Notifier interface {
-	NotifyEventCreation(event *models.Event) error
-	NotifySectionAddition(section *models.Section) error
-}
